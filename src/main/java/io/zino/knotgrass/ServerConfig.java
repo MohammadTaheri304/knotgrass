6bf6318039833d51c81ec8f5b9844e75bfd4c3e2
Seyed M. T. Otaghsara <mohammadtaheri304@gmail.com>
2019-07-11 12:07:05 +0430

    Add server config class

---
+++ src/main/java/io/zino/knotgrass/ServerConfig.java
@@ -0,0 +1,23 @@
+
+
+pacakge io.zino.knotgrass;
+
+public class ServerConfig{
+	private String uuid;
+        private String url;
+        private Integer port;
+        private String sign;
+
+        ServrConfig(String uuid, String url, Integer port, String sign){
+        	this.uuid = uuid;
+                this.url = url;
+                this.port = port;
+                this.sign = sign;
+	}
+
+        public String getUuid(){return this.uuid;}
+        public String getUrl(){return this.url;}
+        public Integer getPort(){return this.port;}
+        public String getSign(){return this.sign;}
+}
+
